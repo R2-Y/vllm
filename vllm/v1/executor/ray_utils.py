8b84cb6128d798fbf0c6fc4f06dc76315299ceac
Lehua Ding <lehuading@tencent.com>
2026-02-06 14:52:00 +0800

    Address cursor comments
    Signed-off-by: Lehua Ding <lehuading@tencent.com>

--- vllm/v1/executor/ray_utils.py
+++ vllm/v1/executor/ray_utils.py
@@ -180,6 +180,7 @@ try:
                 self._execute_model_outputs.append(output)
                 return None
 
+            assert not isinstance(output, AsyncModelRunnerOutput)
             return output
 
         def get_execute_model_output(
@@ -290,7 +291,7 @@ class FutureWrapper(Future):
         return self.ref_or_refs
 
     def result(self, timeout=None):
-        outputs = ray.get(self.get_refs(), timeout=timeout)
+        outputs = ray.get(self.get_refs(timeout), timeout=timeout)
         if self.aggregator is None:
             detach_zero_copy_from_model_runner_output(outputs)
             return outputs
